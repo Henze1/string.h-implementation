688321356ba983996fe0c3def8d68a348c6ee060
Henze1 <podratchyanhenzel@gmail.com>
2025-03-27 15:21:16 +0400

    minor chnages

--- build/CMakeFiles/start.dir/ft_strcpy.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/start.dir/ft_strcpy.c.o: \
- /home/henzel/Documents/textDocs/C/string_h_implementation/ft_strcpy.c \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/start.dir/ft_strncpy.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/start.dir/ft_strncpy.c.o: \
- /home/henzel/Documents/textDocs/C/string_h_implementation/ft_strncpy.c \
- /usr/include/stdc-predef.h
